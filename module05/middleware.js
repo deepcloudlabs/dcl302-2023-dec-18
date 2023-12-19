4af6bb225d84388a661730021fde95ed51ab762c
deepcloudlabs <deepcloudlabs@gmail.com>
2023-12-19 14:36:59 +0300

    Update middleware.js

--- module05/middleware.js
+++ module05/middleware.js
@@ -1,25 +1,24 @@
+// curl -X POST "http://localhost:7100" -d "{\"lotteryMax\": 80, \"lotterySize\": 10}"
 const http = require("http");
 const port = 7100;
 
-// lottery
-function drawLotteryNumbers(max, size) {
-    const numbers = [];
-    while (numbers.length < size) {
-        const candidate = Math.floor(Math.random() * max + 1);
-        if (numbers.includes(candidate)) continue;
-        numbers.push(candidate);
-    }
-    numbers.sort((x,y)=>x-y);
-    return numbers;
-}
+const {getLotteryNumbers, drawLotteryNumbers} = require("./lottery");
+const {Employee,MIN_WAGE} = require("./employee");
 
 function handleRequest(req, res) {
     req.on("data", requestBody => {
         let body = JSON.parse(requestBody.toString());
-        if (body.hasOwnProperty('lotteryMax') && body.hasOwnProperty('lotterySize')) {
+        if (body.hasOwnProperty('lotteryMax')
+            && body.hasOwnProperty('lotterySize')
+            && body.hasOwnProperty('column')
+        ) {
             const lotteryMax = body.lotteryMax;
             const lotterySize = body.lotterySize;
-            const numbers = drawLotteryNumbers(lotteryMax,lotterySize);
+            const column = body.column;
+            const numbers = getLotteryNumbers(lotteryMax, lotterySize, column);
+            numbers.push(drawLotteryNumbers(lotteryMax, lotterySize));
+            numbers.push(new Employee("1", "jack bauer", 100_000, ["IT", "SALES"]));
+            numbers.push({value: MIN_WAGE});
             res.writeHead(200, {"Content-Type": "application/json"});
             res.write(JSON.stringify(numbers));
             res.end();
