889588d2d135336c1c5e34cfbc571bcc695524c6
deepcloudlabs <deepcloudlabs@gmail.com>
2023-12-20 10:59:09 +0300

    Update hr-api.js

--- hr-backend/hr-api.js
+++ hr-backend/hr-api.js
@@ -1,13 +1,16 @@
 const {connect, Schema, model} = require("mongoose");
 const {ibanValidator, tcKimlikNoValidator} = require("./validations");
 
+//region MongoDB Connection
 connect(
     "mongodb://127.0.0.1:27017/hrdb",
     {
         "socketTimeoutMS": 0
     }
 ).then(() => console.log("Connected to the database"));
+//endregion
 
+//region Define Schema
 const departmentSchema = new Schema({
     "name": {
         type: String,
@@ -46,5 +49,26 @@ const employeeSchema = new Schema({
     },
     "department": departmentSchema
 });
+//endregion
 
 const Employee = model("employees", employeeSchema);
+
+//region API Configuration
+const PORT=7100;
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const bodyParser = require("body-parser");
+const api = express();
+const swaggerApiDoc =require("./resources/swagger-hr.json");
+
+api.use(logger("dev"));
+api.use(cors({origin: "*"}));
+api.use(bodyParser.json({limit: "16m"}))
+api.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerApiDoc))
+//endregion
+
+api.listen(PORT,()=>{
+    console.log(`HR Rest API is running at port (${PORT})...`);
+})
